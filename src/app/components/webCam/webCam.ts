0e2b5b753819573e0f48aebdb3629e0b372168c5
Gaurav Sonkusare <gaurav.sonkusare@tietoevry.com>
2023-04-13 20:14:02 +0530

    remove toastr and other as package was not installed and gives error when building

--- src/app/components/webCam/webCam.ts
+++ src/app/components/webCam/webCam.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Subject, Observable, BehaviorSubject } from 'rxjs';
 import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { ToastrService } from 'ngx-toastr';
+// import { IImageInput } from 'src/app/modal/imageInput';
+// import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-webcam',
@@ -16,10 +17,10 @@ export class WebCamComponent implements OnInit {
   private trigger = new Subject();
   private nextWebcam = new Subject();
 
-  localApiUrl = 'https://localhost:7209/api/ImageAnalysis';
-  serverApiUrl = 'https://imageanalysisapi.azurewebsites.net/api/ImageAnalysis';
+  private localApiUrl = 'https://localhost:7209/api/ImageAnalysis';
+  private serverApiUrl = 'https://imageanalysisapi.azurewebsites.net/api/ImageAnalysis';
 
-  constructor(private http: HttpClient, private toastr: ToastrService) {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {}
   public getSnapshot(): void {
@@ -33,15 +34,22 @@ export class WebCamComponent implements OnInit {
   public invokeObservable(): Observable<any> {
     return this.trigger.asObservable();
   }
+
   public nextWebcamObservable(): Observable<any> {
     return this.nextWebcam.asObservable();
   }
 
-  fetchDetailsForCapturedImage() {
+  public onFetchDetailsForCapturedImage() {
     if (this.captureImage) {
+      let convertedUrlBlob = null;
+      convertedUrlBlob = this.dataURItoBlob(this.captureImage);
+
+      const formData = new FormData(document.forms[0]);
+      formData.append('imagePath', convertedUrlBlob);
+
       const headers = new HttpHeaders();
 
-      headers.set('Content-Type', 'multipart/form-data');
+      headers.set('Content-Type', 'application/json');
       headers.set('Accept', 'application/json');
 
       headers.set('Access-Control-Allow-Methods', 'Content-Type');
@@ -49,11 +57,11 @@ export class WebCamComponent implements OnInit {
       headers.set('Access-Control-Allow-Origin', 'http://localhost:4200/');
       // headers.set('Access-Control-Allow-Methods', 'Authorization');
 
-      var input = {
-        imagePath: 'C:\\Images\\500front.png',
-      };
+      // const input: IImageInput = {
+      //   imagePath: 'C:\\Images\\500front.png',
+      // };
 
-      const req = this.http.post(this.localApiUrl, input, {
+      const req = this.http.post(this.localApiUrl, formData, {
         headers,
         reportProgress: true,
         responseType: 'json',
@@ -64,15 +72,36 @@ export class WebCamComponent implements OnInit {
       // });
       return req.subscribe((data: any) => {
         if (data.success) {
-          this.toastr.success(data.message);
+          // this.toaster.success(data.message);
         }
 
         if (!data.success) {
-          this.toastr.error(data.message);
+          // this.toaster.error(data.message);
         }
       });
     } else {
-      this.toastr.error('Please scan image first. !!!');
+      // this.toaster.error('Please scan image first. !!!');
     }
+  }
+
+  private dataURItoBlob(dataURI): Blob {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    let byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+      byteString = atob(dataURI.split(',')[1]);
+    } else {
+      byteString = unescape(dataURI.split(',')[1]);
+    }
+
+    // separate out the mime component
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    const ia = new Uint8Array(byteString.length);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], { type: mimeString });
   }
 }
