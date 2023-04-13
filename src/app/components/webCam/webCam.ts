bc29eaf4d882b93f630d6b7350d78fbcc7e485bd
Gaurav Sonkusare <gaurav.sonkusare@tietoevry.com>
2023-04-13 22:30:32 +0530

    next try

--- src/app/components/webCam/webCam.ts
+++ src/app/components/webCam/webCam.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Subject, Observable, BehaviorSubject } from 'rxjs';
 import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-// import { IImageInput } from 'src/app/modal/imageInput';
+import { IImageInput } from 'src/app/modal/imageInput';
 // import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -19,6 +19,8 @@ export class WebCamComponent implements OnInit {
 
   private localApiUrl = 'https://localhost:7209/api/ImageAnalysis';
   // private serverApiUrl = 'https://imageanalysisapi.azurewebsites.net/api/ImageAnalysis';
+
+  private image;
 
   constructor(private http: HttpClient) {}
 
@@ -39,48 +41,36 @@ export class WebCamComponent implements OnInit {
     return this.nextWebcam.asObservable();
   }
 
+  saveImage(event: any) {
+    const target = event.target.files;
+    console.log(target);
+  }
+
+  // saveImage(files: FileList) {
+  //   const file: File = files.item(0);
+  //   const myReader: FileReader = new FileReader();
+
+  //   myReader.onloadend = (e) => {
+  //     this.image = myReader.result;
+  //   };
+
+  //   myReader.readAsDataURL(file);
+  // }
+
   public onFetchDetailsForCapturedImage() {
-    if (this.captureImage) {
+    const imageURl = '/assets/icon/favicon.png';
+    const input: IImageInput = { imagePath: imageURl };
+
+    if (!this.captureImage) {
+      // this.toaster.error('Please scan image first. !!!');
+    } else {
       let convertedUrlBlob = null;
       convertedUrlBlob = this.dataURItoBlob(this.captureImage);
 
-      const formData = new FormData(document.forms[0]);
-      formData.append('imagePath', convertedUrlBlob);
+      const formData = new FormData();
+      formData.set('imagePath', convertedUrlBlob);
 
-      const headers = new HttpHeaders();
-
-      headers.set('Content-Type', 'application/json');
-      headers.set('Accept', 'application/json');
-
-      headers.set('Access-Control-Allow-Methods', 'Content-Type');
-      headers.set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-      headers.set('Access-Control-Allow-Origin', 'http://localhost:4200/');
-      // headers.set('Access-Control-Allow-Methods', 'Authorization');
-
-      // const input: IImageInput = {
-      //   imagePath: 'C:\\Images\\500front.png',
-      // };
-
-      const req = this.http.post(this.localApiUrl, formData, {
-        headers,
-        reportProgress: true,
-        responseType: 'json',
-      });
-      //.subscribe((data) => {
-      //Check success message
-      //  console.log(data);
-      // });
-      return req.subscribe((data: any) => {
-        if (data.success) {
-          //this.toaster.success(data.message);
-        }
-
-        if (!data.success) {
-          //this.toaster.error(data.message);
-        }
-      });
-    } else {
-     // this.toaster.error('Please scan image first. !!!');
+      this.onPost(formData);
     }
   }
 
@@ -89,11 +79,12 @@ export class WebCamComponent implements OnInit {
     let byteString;
     if (dataURI.split(',')[0].indexOf('base64') >= 0) {
       byteString = atob(dataURI.split(',')[1]);
-    } else {
-      byteString = unescape(dataURI.split(',')[1]);
     }
+    // else {
+    //   byteString = unescape(dataURI.split(',')[1]);
+    // }
 
-    // separate out the mime component
+    // separate out the mime component.
     const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
     // write the bytes of the string to a typed array
@@ -102,6 +93,24 @@ export class WebCamComponent implements OnInit {
       ia[i] = byteString.charCodeAt(i);
     }
 
-    return new Blob([ia], { type: mimeString });
+    return new Blob([ia], { type: mimeString }); // returns size and type of img.
+  }
+
+  private onPost(input: any) {
+    const headers = new HttpHeaders({
+      contentType: 'application/json',
+      accept: 'undefined',
+    });
+
+    const req = this.http.post(this.localApiUrl, input, { headers });
+    return req.subscribe((data: any) => {
+      if (data.success) {
+        //this.toaster.success(data.message);
+      }
+
+      if (!data.success) {
+        //this.toaster.error(data.message);
+      }
+    });
   }
 }
