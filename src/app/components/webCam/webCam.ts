5484964c871516ab4d8e50f6a1ab646c96dbc64d
pandjpra <prajakta.pandit@tietoevry.com>
2023-04-14 17:14:49 +0530

    File saver changes

--- src/app/components/webCam/webCam.ts
+++ src/app/components/webCam/webCam.ts
@@ -4,6 +4,15 @@ import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
 import { IImageInput } from 'src/app/modal/imageInput';
+ //var FileSaver = require('filesaver');
+ import { saveAs } from 'file-saver';
+//var fs = require('fs');
+//import { writeFile } from 'fs';
+var FileSaver = require('file-saver');
+
+var folders = {
+  images: 'C:\\Images\\'
+}
 
 @Component({
   selector: 'app-webcam',
@@ -13,10 +22,9 @@ import { IImageInput } from 'src/app/modal/imageInput';
 export class WebCamComponent implements OnInit {
   public webcamImage: WebcamImage;
   public captureImage = '';
-
   private trigger = new Subject();
   private nextWebcam = new Subject();
-  private input: IImageInput = { imagePath: 'C:\\Images\\500front.png' };
+  private input: IImageInput = { imagePath: 'C:\\Images\\500back.jpg' };
 
   private localApiUrl = 'https://localhost:7209/api/ImageAnalysis';
   private serverApiUrl =
@@ -34,6 +42,12 @@ export class WebCamComponent implements OnInit {
     this.webcamImage = webcamImage;
     this.captureImage = webcamImage?.imageAsDataUrl;
     this.base64img = btoa(this.captureImage);
+    
+    var tempblob : Blob = this.dataURItoBlob(this.captureImage);
+    var blob = new Blob([tempblob], {type: "image/jpeg"});
+    //var tempFile = new File([tempblob], event.target.files[0]);
+    FileSaver.saveAs(blob, "1.jpg");
+    //this.savePicture(webcamImage);
   }
 
   public invokeObservable(): Observable<any> {
@@ -107,5 +121,22 @@ export class WebCamComponent implements OnInit {
     headers.set('Access-Control-Allow-Methods', 'Content-Type');
     // headers.set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
     return headers;
+  }
+
+  private dataURItoBlob(dataURI): Blob {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    let byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+      byteString = atob(dataURI.split(',')[1]);
+       
+      const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+      const ia = new Uint8Array(byteString.length);
+      for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      return new Blob([ia], { type: mimeString }); // returns size and type of img.
+}
   }
 }
