fd751194db617e03a65ac8f92838e983216aeb6b
Gaurav Sonkusare <gaurav.sonkusare@tietoevry.com>
2023-04-17 13:03:23 +0530

    local image testing

--- src/app/components/webCam/webCam.ts
+++ src/app/components/webCam/webCam.ts
@@ -4,15 +4,7 @@ import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
 import { IImageInput } from 'src/app/modal/imageInput';
- //var FileSaver = require('filesaver');
- import { saveAs } from 'file-saver';
-//var fs = require('fs');
-//import { writeFile } from 'fs';
-var FileSaver = require('file-saver');
-
-var folders = {
-  images: 'C:\\Images\\'
-}
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-webcam',
@@ -24,16 +16,15 @@ export class WebCamComponent implements OnInit {
   public captureImage = '';
   private trigger = new Subject();
   private nextWebcam = new Subject();
-  private input: IImageInput = { imagePath: 'C:\\Images\\500back.jpg' };
+  // private input: IImageInput = { imagePath: 'C:\\Images\\500back.jpg' };
 
   private localApiUrl = 'https://localhost:7209/api/ImageAnalysis';
   private serverApiUrl =
     'https://imageanalysisapi.azurewebsites.net/api/ImageAnalysis';
   private base64img: string = null;
+  private base64textString: string = null;
 
-  constructor(private http: HttpClient, private toastr: ToastrService) {
-  
-}
+  constructor(private http: HttpClient, private toastr: ToastrService) {}
 
   ngOnInit() {}
 
@@ -43,13 +34,12 @@ export class WebCamComponent implements OnInit {
   public onCaptureImgClick(webcamImage: WebcamImage): void {
     this.webcamImage = webcamImage;
     this.captureImage = webcamImage?.imageAsDataUrl;
-    this.base64img = (webcamImage?.imageAsBase64);
+    this.base64img = webcamImage?.imageAsBase64;
 
-    
     // var file = "C:\\Images\\500back.jpg";
     // var reader = new FileReader();
     // reader.readAsBinaryString(file);
-    
+
     // var tempblob : Blob = this.dataURItoBlob(this.captureImage);
     // var blob = new Blob([tempblob], {type: "image/jpeg"});
     // //var tempFile = new File([tempblob], event.target.files[0]);
@@ -57,43 +47,36 @@ export class WebCamComponent implements OnInit {
     //this.savePicture(webcamImage);
   }
 
-  private base64textString:String="";
-  
-  handleFileSelect(evt){
-      var files = evt.target.files;
-      var file = files[0];
-    
+  handleFileSelect(evt) {
+    let files = null;
+    files = evt.target.files;
+    const file = files[0];
+
     if (files && file) {
-
-        var reader = new FileReader();
-
-        reader.onload =this._handleReaderLoaded.bind(this);
-
-        //reader.readAsBinaryString(file);
-        reader.readAsDataURL(file);
+      const reader = new FileReader();
+      reader.onload = this.handleReaderLoaded.bind(this);
+      //// reader.readAsBinaryString(file);
+      reader.readAsDataURL(file);
     }
   }
-  
-  _handleReaderLoaded(readerEvt) {
-     var binaryString = readerEvt.target.result;
-            this.base64textString= btoa(binaryString);
-            console.log(btoa(binaryString));
-    }
-  
+
+  handleReaderLoaded(readerEvt) {
+    const binaryString = readerEvt.target.result;
+    this.base64textString = btoa(binaryString);
+    console.log(btoa(this.base64textString));
+  }
+
   public getBase64(event) {
-    let me = this;
-    let file = event.target.files[0];
-    //let file = "C:\\Images\\500back.jpg";
-    let reader = new FileReader();
+    const file = event.target.files[0];
+    const reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.onload = function () {
-      //me.modelvalue = reader.result;
+    reader.onload = () => {
       console.log(reader.result);
     };
-    reader.onerror = function (error) {
+    reader.onerror = (error) => {
       console.log('Error: ', error);
     };
- }
+  }
 
   public invokeObservable(): Observable<any> {
     return this.trigger.asObservable();
@@ -106,16 +89,14 @@ export class WebCamComponent implements OnInit {
   public onFetchDetailsForCapturedImage() {
     if (this.captureImage) {
       const headers = this.requestHeader();
-      
-      // const input = {
-      //   imagePath: 'C:\\Images\\500front.png',
-      // };
+
+      // const input = { imagePath: 'C:\\Images\\500front.png', };
 
       const input = {
         imagePath: this.base64img,
       };
 
-      const req = this.http.post(this.localApiUrl, input, { headers, });
+      const req = this.http.post(this.localApiUrl, input, { headers });
 
       return req.subscribe((data: any) => {
         if (data.success) {
@@ -139,9 +120,13 @@ export class WebCamComponent implements OnInit {
   public identifyCurrencyImageSide() {
     if (this.captureImage) {
       const headers = this.requestHeader();
-      const param = '?imagePath=' + this.input.imagePath;
+      // const param = '?imagePath=' + this.base64img;
 
-      const getReq = this.http.get(this.localApiUrl + param, { headers, });
+      // const input = {
+      //   imagePath: this.base64img,
+      // };
+
+      const getReq = this.http.get(this.localApiUrl + this.base64img, { headers });
 
       return getReq.subscribe((data: any) => {
         if (data.success) {
@@ -159,29 +144,27 @@ export class WebCamComponent implements OnInit {
 
   private requestHeader() {
     const headers = new HttpHeaders();
-
     headers.set('Content-Type', 'multipart/form-data');
-    // headers.set('Accept', 'application/json');
-
+   //// headers.set('Accept', 'application/json');
     headers.set('Access-Control-Allow-Methods', 'Content-Type');
-    // headers.set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  ////  headers.set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
     return headers;
   }
 
-  private dataURItoBlob(dataURI): Blob {
-    // convert base64/URLEncoded data component to raw binary data held in a string
-    let byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0) {
-      byteString = atob(dataURI.split(',')[1]);
-       
-      const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+  // // private dataURItoBlob(dataURI): Blob {
+  // //   // convert base64/URLEncoded data component to raw binary data held in a string
+  // //   let byteString;
+  // //   if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+  // //     byteString = atob(dataURI.split(',')[1]);
 
-      const ia = new Uint8Array(byteString.length);
-      for (let i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-      }
+  // //     const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
-      return new Blob([ia], { type: mimeString }); // returns size and type of img.
-}
-  }
+  // //     const ia = new Uint8Array(byteString.length);
+  // //     for (let i = 0; i < byteString.length; i++) {
+  // //       ia[i] = byteString.charCodeAt(i);
+  // //     }
+
+  // //     return new Blob([ia], { type: mimeString }); // returns size and type of img.
+  // //   }
+  // // }
 }
